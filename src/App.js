94738d4d6fdd9c0c644988b1389b281746700e09
ankur-b <barveankur29@outlook.com>
2021-08-05 13:18:30 +0530

    add Accordian and Search Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Accordion from './Components/Accordion'
+import Search from './Components/Search'
 function App() {
+  const items=[{title:'t1',
+  content:'c1'
+},{
+  title:'t2',
+  content:'c2'
+},{
+  title:'t3',
+  content:'c3'
+}]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Accordion items={items}/> */}
+      <Search/>
     </div>
   );
 }
