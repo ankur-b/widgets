cd415eea83fdb4ddcd8cc4e3f623758749ac98fb
ankur-b <barveankur29@outlook.com>
2021-08-18 08:26:43 +0530

    functionality added in dropdown component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
-import React ,{useState} from 'react'
-import Accordion from './Components/Accordion'
-import Search from './Components/Search'
-import Dropdown from './Components/Dropdown'
-const App=()=> {
-  const items=[{title:'t1',
-  content:'c1'
-},{
-  title:'t2',
-  content:'c2'
-},{
-  title:'t3',
-  content:'c3'
-}]
-  const options=[
-    {label:"The Color Red",value:"red"},
-    {label:"The Color Green",value:"green"},
-    {label:"A Shade of Blue",value:"blue"}
-  ]
-  const [selected,setSelected] = useState(options[0])
+import React, { useState } from "react";
+import Accordion from "./Components/Accordion";
+import Search from "./Components/Search";
+import Dropdown from "./Components/Dropdown";
+const App = () => {
+  const items = [
+    { title: "t1", content: "c1" },
+    {
+      title: "t2",
+      content: "c2",
+    },
+    {
+      title: "t3",
+      content: "c3",
+    },
+  ];
+  const options = [
+    { label: "The Color Red", value: "red" },
+    { label: "The Color Green", value: "green" },
+    { label: "A Shade of Blue", value: "blue" },
+  ];
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
   return (
     <div className="App">
+      <button
+        onClick={() => {
+          setShowDropdown(!showDropdown);
+        }}
+      >
+        Toggle Dropdown
+      </button>
       {/* <Accordion items={items}/> */}
       {/* <Search/> */}
-      <Dropdown selected={selected} onSelectChange={setSelected} options={options}/>
+      {showDropdown ? (
+        <Dropdown
+          selected={selected}
+          onSelectChange={setSelected}
+          options={options}
+        />
+      ) : null}
     </div>
   );
-}
+};
 
 export default App;
